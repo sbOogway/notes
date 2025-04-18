fcabe6ce8189b205090a1cd78e85d39d77f3927f
oogway <mattiapapaccioli@gmail.com>
2025-04-18 12:30:50 +0200

    build Fri Apr 18 12:30:50 PM CEST 2025

--- fetch.sh
+++ fetch.sh
@@ -1,6 +1,7 @@
 cp ../micro/docs/notes.pdf micro.pdf
 cp ../prog_func/notes.pdf prog_func.pdf
 cp ../prog_proc/notes.svg prog_proc.svg
+cp ../automi/notes.pdf automi.pdf
 
 git add .
 git commit -m "build $(date)" 
