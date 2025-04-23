a8e3f06ebc1a540e17f7bfb3b38653414e6a8440
oogway <mattiapapaccioli@gmail.com>
2025-04-23 19:50:51 +0200

    Wed Apr 23 07:50:51 PM CEST 2025

--- fetch.sh
+++ fetch.sh
@@ -1,8 +1,47 @@
-cp ../micro/docs/notes.pdf micro.pdf
-cp ../prog_func/notes.pdf prog_func.pdf
-cp ../prog_proc/notes.svg prog_proc.svg
-cp ../automi/notes.pdf automi.pdf
+#!/bin/bash
+
+prefix='/home/oogway/uni/'
+declare -A materie
+materie[micro]='microcontrollori 💡' 
+materie[prog_proc]='programmazione_procedurale 🇨' 
+materie[prog_func]='programmazione_funzionale λ'
+materie[automi]='automi_e_linguaggi 🏯'
+
+emojis=''
+html_index=''
+li='<a href="%s.pdf" target="_blank" rel="noopener noreferrer"><li>%s</li></a>'
+
+for materia in "${!materie[@]}"
+do  
+    # echo $materia
+    IFS=' ' read -ra data <<< "${materie[$materia]}"
+    # echo ${data[1]}
+    # exit
+    cd "$prefix$materia" 
+    # pwd
+    # exit
+    make docs &> /dev/null
+    cd "${prefix}notes"
+    cp "${prefix}${materia}/notes.pdf" "docs/$materia.pdf"
+    li_iter="$(printf "${li}\n" "$materia" "${data[0]}")"
+    html_index="${html_index}$li_iter"
+    emojis="${emojis}${data[1]} "
+    # echo $li_iter
+    # exit
+    
+done
+
+# echo $html_index
+# echo $emojis
+# exit
+# printf "$html_index" "\n"
+# printf "$emojis" ""
+
+template="$(cat index-template.html)"
+printf "$template" "$emojis" "$html_index" > index.html
+# echo template
+
 
 git add .
-git commit -m "build $(date)" 
+git commit -m "$(date)" 
 git push
